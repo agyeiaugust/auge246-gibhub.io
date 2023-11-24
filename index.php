595ecc3b8086a1ddb8f2fd5bcb762cad0a6e3fe1
agyeiaugust <123258539+agyeiaugust@users.noreply.github.com>
2023-11-24 23:04:23 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>CRM Project using PHP and MySQL</title>
+
+       
+
+            <button class="nav-item">
+            <a class="nav-link" href="admin/">Start Auge snake game</a>
+          </button>
+        </ul>
+      </div>
+    </div>
+  </body>
+
+</html>
